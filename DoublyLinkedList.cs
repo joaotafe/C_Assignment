502fea3337cd6f2eb162fed24836463f70dadd78
joaotafe <joao.souza@student.tafesa.edu.au>
2023-11-28 14:25:06 +1030

    Code develope on both classes.

--- DoublyLinkedList.cs
+++ DoublyLinkedList.cs
@@ -6,7 +6,102 @@ using System.Threading.Tasks;
 
 namespace Assignment2
 {
-    internal class DoublyLinkedList
+    public class DoublyLinkedList<T>
     {
+        public DoublyLinkedListNode<T> Head { get; private set; }
+        public DoublyLinkedListNode<T> Tail { get; private set; }
+
+        public DoublyLinkedList()
+        {
+            Head = null;
+            Tail = null;
+        }
+
+        public void InsertAtHead(T data)
+        {
+            var newNode = new DoublyLinkedListNode<T>(data);
+            if (Head == null)
+            {
+                Head = newNode;
+                Tail = newNode;
+            }
+            else
+            {
+                newNode.Next = Head;
+                Head.Prev = newNode;
+                Head = newNode;
+            }
+        }
+
+        public void InsertAtTail(T data)
+        {
+            var newNode = new DoublyLinkedListNode<T>(data);
+            if (Tail == null)
+            {
+                Head = newNode;
+                Tail = newNode;
+            }
+            else
+            {
+                Tail.Next = newNode;
+                newNode.Prev = Tail;
+                Tail = newNode;
+            }
+        }
+
+        public bool DeleteNode(T data)
+        {
+            var current = Head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    if (current.Prev != null)
+                    {
+                        current.Prev.Next = current.Next;
+                    }
+                    else
+                    {
+                        Head = current.Next;
+                    }
+
+                    if (current.Next != null)
+                    {
+                        current.Next.Prev = current.Prev;
+                    }
+                    else
+                    {
+                        Tail = current.Prev;
+                    }
+
+                    return true; // Node found and deleted
+                }
+                current = current.Next;
+            }
+            return false; // Node not found
+        }
+
+        public DoublyLinkedListNode<T> Find(T data)
+        {
+            var current = Head;
+            while (current != null)
+            {
+                if (current.Data.Equals(data))
+                    return current;
+
+                current = current.Next;
+            }
+            return null;
+        }
+
+        public void Traverse()
+        {
+            var current = Head;
+            while (current != null)
+            {
+                Console.WriteLine(current.Data);
+                current = current.Next;
+            }
+        }
     }
 }
--- DoublyLinkedListNode.cs
+++ DoublyLinkedListNode.cs
@@ -1,7 +1,16 @@
 ﻿namespace Assignment2
 {
-    public class Class1
+    public class DoublyLinkedListNode<T>
     {
+        public T Data { get; set; }
+        public DoublyLinkedListNode<T> Next { get; set; }
+        public DoublyLinkedListNode<T> Prev { get; set; }
 
+        public DoublyLinkedListNode(T data)
+        {
+            this.Data = data;
+            this.Next = null;
+            this.Prev = null;
+        }
     }
 }
